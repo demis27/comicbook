0c6d202b9e75b9269d6cdb71033ac58f62ddb283
skermabon <skermabon@talend.com>
2018-09-25 17:32:34 +0200

    docker-compose 0

---
+++ docker-compose/postgresql/init-db.sql
@@ -0,0 +1,15 @@
+create table comicbook (
+	 id 						varchar(32)
+	,title						varchar(256)
+	
+);
+
+create table person (
+	 id 						varchar(32)
+	,firstname					varchar(32)
+	,lastname					varchar(32)
+	,middlename					varchar(32)
+	,creation_date				date
+	,last_modification_date		date
+);
+
