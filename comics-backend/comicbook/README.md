d155dca04ca7163110da882fde93f49519190dd7
skermabon <skermabon@talend.com>
2018-10-03 10:42:19 +0200

    add comicbook service

---
+++ comics-backend/comicbook/README.md
@@ -0,0 +1,9 @@
+# Comicbook service
+
+## build
+mvn package
+
+## launch
+mvn spring-boot:run
+
+java -jar target/comicbook-0.0.1-SNAPSHOT.jar
---
+++ comics-backend/comicbook/src/main/java/org/demis27/comics/comicbook/ComicbookApplication.java
@@ -0,0 +1,12 @@
+package org.demis27.comics.comicbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicbookApplication.class, args);
+	}
+}
