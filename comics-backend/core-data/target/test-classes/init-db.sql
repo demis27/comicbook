c301c723b01f383b56e85dd3feeb3c79f891f7c7
Stéphane Kermabon <demis27@demis27.net>
2018-10-26 13:25:12 +0200

    first JPA unit tests

--- comics-backend/core-data/target/test-classes/init-db.sql
+++
@@ -1,7 +0,0 @@
-create table comic_book (
-	 comic_book_id 					varchar(36)
-	,title							varchar(256)
-	,creation_date               	timestamp without time zone default current_date not null
-	,modification_date 				timestamp without time zone default current_date not null
-	, constraint comic_book_pk primary key (comic_book_id)
-);
--- docker-compose/postgresql/init-db.sql
+++ docker-compose/postgresql/init-db.sql
@@ -1,5 +1,5 @@
 create table comic_book (
-	 comic_book_id 					varchar(32)
+	 comic_book_id 					varchar(64)
 	,title							varchar(256)
 	,creation_date               	timestamp without time zone default current_date not null
 	,modification_date 				timestamp without time zone default current_date not null	
